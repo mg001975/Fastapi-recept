410b98cba782d6fff58a02259eb512c3e26e94a3
mg001975 <87535948+mg001975@users.noreply.github.com>
2025-12-05 16:28:17 +0100

    added uv

--- auth/auth_utils.py
+++ auth/auth_utils.py
@@ -1,16 +1,15 @@
 from passlib.context import CryptContext
 
-# Initialize the password context (for hashing)
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt_sha256"], deprecated="auto")
 
 
 # Function to hash the password
 def hash_password(password: str) -> str:
+    password = password.encode("utf-8")[:72]
     return pwd_context.hash(password)
 
 
 # Function to verify the hashed password
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    print(hashed_password)
-    print(plain_password)
+    plain_password = plain_password.encode("utf-8")[:72]
     return pwd_context.verify(plain_password, hashed_password)
